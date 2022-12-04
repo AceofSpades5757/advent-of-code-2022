2ba72abbfc98dfd8cfabb3b88c8e629358586769
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-12-03 23:10:34 -0600

    Finish day 4 puzzle

---
+++ day-04/src/bin/part_1.rs
@@ -0,0 +1,55 @@
+/// Solve the Puzzle
+fn solve(input: &str) -> i32 {
+    let lines: Vec<String> = input.lines().map(|s| s.to_owned()).collect();
+
+    let mut total = 0;
+    for pair in lines.into_iter() {
+        let first_elf = pair.split(",").next().unwrap();
+        let second_elf = pair.split(",").last().unwrap();
+
+        let first_lower = first_elf.split("-").next().unwrap().parse::<i32>().unwrap();
+        let first_upper = first_elf.split("-").last().unwrap().parse::<i32>().unwrap();
+        let second_lower = second_elf.split("-").next().unwrap().parse::<i32>().unwrap();
+        let second_upper = second_elf.split("-").last().unwrap().parse::<i32>().unwrap();
+        let first_range = first_lower..=first_upper;
+        let second_range = second_lower..=second_upper;
+
+        if first_range.contains(&second_lower) && first_range.contains(&second_upper) {
+            total += 1;
+        } else if second_range.contains(&first_lower) && second_range.contains(&first_upper) {
+            total += 1;
+        }
+    }
+
+    total
+}
+
+fn main() {
+    let input = std::fs::read_to_string("./input.txt").expect(
+        "Input file exists and is readable in the root member's directory: day-XX/input.txt",
+    );
+    let answer = solve(&input);
+    println!("{}", answer);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[allow(unused_imports)]
+    use pretty_assertions::{assert_eq, assert_ne};
+
+    #[test]
+    fn test_compute() {
+        let tests = vec![
+            ("2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8", 2)
+        ];
+        for (input, expected) in tests {
+            assert_eq!(solve(input), expected);
+        }
+    }
+}
