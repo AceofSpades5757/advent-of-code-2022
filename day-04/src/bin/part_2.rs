443891244e6c5625fa76f576e2fd521805bcbaf8
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2022-12-05 21:00:17 -0600

    Reiterate on day 2
    Use nom to parse the input

--- day-04/src/bin/part_2.rs
+++ day-04/src/bin/part_2.rs
@@ -1,37 +1,58 @@
+use nom::bytes::complete::tag;
+use nom::character::complete::digit1;
+use nom::character::complete::newline;
+use nom::combinator::map_res;
+use nom::combinator::opt;
+use nom::multi::many1;
+use nom::sequence::separated_pair;
+use nom::sequence::terminated;
+use nom::IResult;
+
 /// Solve the Puzzle
 fn solve(input: &str) -> i32 {
-    let lines: Vec<String> = input.lines().map(|s| s.to_owned()).collect();
+    let (_, pairs) = parse_file(input).unwrap();
 
-    let mut total = 0;
-    for pair in lines.into_iter() {
-        let first_elf = pair.split(",").next().unwrap();
-        let second_elf = pair.split(",").last().unwrap();
-
-        let first_lower = first_elf.split("-").next().unwrap().parse::<i32>().unwrap();
-        let first_upper = first_elf.split("-").last().unwrap().parse::<i32>().unwrap();
-        let second_lower = second_elf
-            .split("-")
-            .next()
-            .unwrap()
-            .parse::<i32>()
-            .unwrap();
-        let second_upper = second_elf
-            .split("-")
-            .last()
-            .unwrap()
-            .parse::<i32>()
-            .unwrap();
-        let first_range = first_lower..=first_upper;
-        let second_range = second_lower..=second_upper;
-
-        if first_range.contains(&second_lower) || first_range.contains(&second_upper) {
-            total += 1;
-        } else if second_range.contains(&first_lower) || second_range.contains(&first_upper) {
-            total += 1;
+    let mut tally = 0;
+    for (elf_1, elf_2) in pairs.iter() {
+        if elf_1.contains(&elf_2.start()) || elf_1.contains(&elf_2.end()) {
+            tally += 1;
+        } else if elf_2.contains(&elf_1.start()) || elf_2.contains(&elf_1.end()) {
+            tally += 1;
         }
     }
+    tally
+}
 
-    total
+fn parse_range(input: &str) -> IResult<&str, std::ops::RangeInclusive<u32>> {
+    Ok(map_res(
+        separated_pair(
+            map_res(digit1, str::parse::<u32>),
+            tag("-"),
+            map_res(digit1, str::parse::<u32>),
+        ),
+        |(l, h)| Ok::<std::ops::RangeInclusive<u32>, String>(l..=h),
+    )(input)?)
+}
+
+fn parse_pair(
+    input: &str,
+) -> IResult<&str, (std::ops::RangeInclusive<u32>, std::ops::RangeInclusive<u32>)> {
+    Ok(separated_pair(parse_range, tag(","), parse_range)(input)?)
+}
+
+fn parse_line(
+    input: &str,
+) -> IResult<&str, (std::ops::RangeInclusive<u32>, std::ops::RangeInclusive<u32>)> {
+    Ok(terminated(parse_pair, opt(newline))(input)?)
+}
+
+/// Get pairs from input file.
+fn parse_file(
+    input: &str,
+) -> IResult<&str, Vec<(std::ops::RangeInclusive<u32>, std::ops::RangeInclusive<u32>)>> {
+    let (input, pairs) = many1(parse_line)(input)?;
+
+    Ok((input, pairs))
 }
 
 fn main() {
